074d689bd07d90a5e11ec88b30ae6ff8fe3cc59a
odifmonster <lamanwyner@gmail.com>
2025-07-31 12:54:19 -0400

    load_inventory function works

--- app/inventory/__init__.py
+++ app/inventory/__init__.py
@@ -2,5 +2,6 @@
 
 from . import groups, roll, style
 from .inventory import Inventory
+from .load_from_file import load_inventory
 
-__all__ = ['groups', 'roll', 'style', 'Inventory']
+__all__ = ['groups', 'roll', 'style', 'Inventory', 'load_inventory']
