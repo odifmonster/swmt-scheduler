7a44513875e39a6522d124cdf668c93e8a3cc321
odifmonster <lamanwyner@gmail.com>
2025-08-10 11:18:55 -0400

    created job submodule

--- app/schedule/__init__.py
+++ app/schedule/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-from . import demand, dyelot
+from . import demand, dyelot, job
 
-__all__ = ['demand', 'dyelot']
+__all__ = ['demand', 'dyelot', 'job']
---
+++ app/schedule/job/job.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import datetime
+from app.support import HasID
+from ..dyelot import DyeLot
+
+_JOB_CTR = 0
+
+_EMPTY_LOT = (DyeLot(),)
+
+class _JobBase(HasID[int]):
+
+    def __init__(self, start: datetime.datetime, cycle_time: datetime.timedelta,
+                 lots: tuple[DyeLot, ...] = _EMPTY_LOT):
+        globals()['_JOB_CTR'] += 1
+        self.__id = globals()['_JOB_CTR']
+
+        self.__start = start
+        self.__cycle_time = cycle_time
+        self.__lots = lots
+
+    @property
+    def _prefix(self):
+        return 'JOB'
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def start(self):
+        return self.__start
+    
+    @property
+    def cycle_time(self):
+        return self.__cycle_time
+    
+    @property
+    def end(self):
+        return self.__start + self.__cycle_time
+    
+    @property
+    def lbs(self):
+        return sum(map(lambda x: x.lbs, self.__lots))
+    
+    @property
+    def yds(self):
+        return sum(map(lambda x: x.yds, self.__lots))
+    
+    def __repr__(self):
+        start_str = self.start.strftime('%m/%d/%y %H:%M:%S')
+        end_str = self.end.strftime('%m/%d/%y %H:%M:%S')
+        return f'{self._prefix}(id={self.id:05}, start={start_str}, end={end_str})'
+    
+class Job(_JobBase):
+    pass
