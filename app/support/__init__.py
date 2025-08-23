3518d7240532d875519dba25e67298d767994fa4
odifmonster <lamanwyner@gmail.com>
2025-08-23 11:36:25 -0400

    removed remaining references to logging

--- app/support/__init__.py
+++ app/support/__init__.py
@@ -3,8 +3,7 @@
 from .supers import *
 from .protocols import *
 from .range import *
-from . import logging
 
 __all__ = ['SuperImmut', 'ArgTup', 'SuperView', 'setter_like',
            'HasID', 'Viewable',
-           'ContRange', 'FloatRange', 'DateRange', 'logging']
+           'ContRange', 'FloatRange', 'DateRange']
